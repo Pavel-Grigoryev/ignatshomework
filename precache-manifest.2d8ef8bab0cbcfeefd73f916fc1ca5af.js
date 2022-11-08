b8ee36eee35a73c9ce0c625cf29b514b1468d47f
Pavel <3935371@gmail.com>
2022-11-08 14:50:47 +0300

    Updates

---
+++ precache-manifest.2d8ef8bab0cbcfeefd73f916fc1ca5af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6b083a1a7c23b8978ca754ed4296966",
+    "url": "/ignatshomework/index.html"
+  },
+  {
+    "revision": "4b07a6095bb1dc88d51a",
+    "url": "/ignatshomework/static/css/main.3ded75e8.chunk.css"
+  },
+  {
+    "revision": "c75392058d7141ca242d",
+    "url": "/ignatshomework/static/js/2.8639d9d4.chunk.js"
+  },
+  {
+    "revision": "19e441aa73bb21f8faa316c23d030462",
+    "url": "/ignatshomework/static/js/2.8639d9d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b07a6095bb1dc88d51a",
+    "url": "/ignatshomework/static/js/main.bf6adb2c.chunk.js"
+  },
+  {
+    "revision": "a6e293521523b51f18c9",
+    "url": "/ignatshomework/static/js/runtime-main.dd65642c.js"
+  },
+  {
+    "revision": "8b8689abb28faee6f1ceffa78803c42b",
+    "url": "/ignatshomework/static/media/arrow.8b8689ab.svg"
+  }
+]);
